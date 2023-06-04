38deb74e0c3e8bf0057848088c9bd3d9989f4877
alex <alekco29@yandex.ru>
2023-06-04 19:48:26 +0300

    Debug False.

--- trust/trust/settings.py
+++ trust/trust/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('KEY_DJANGO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["185.241.54.222", "localhost", "trustservice.zapto.org"]
 
